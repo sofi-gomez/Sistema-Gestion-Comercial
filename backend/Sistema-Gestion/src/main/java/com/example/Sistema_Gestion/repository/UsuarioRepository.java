2efb461a24d55c1a2407cf9a4174656a0115f077
Sofi Gomez <ailenmumigomez@gmail.com>
2026-03-13 21:32:46 -0300

    back: agrego modelo de Usuario, Repositorio y manejo global de excepciones

---
+++ backend/Sistema-Gestion/src/main/java/com/example/Sistema_Gestion/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.Sistema_Gestion.repository;
+
+import com.example.Sistema_Gestion.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
